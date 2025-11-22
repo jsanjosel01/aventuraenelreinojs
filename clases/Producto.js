ea545ceeb1737b35e823a2563f0d9ad033ec4e0d
jsanjosel01 <jsanjosel01@iesalbarregas.es>
2025-11-22 14:37:56 +0100

    ✅New create: Creación del mercado y los productos.

---
+++ clases/Producto.js
@@ -0,0 +1,34 @@
+// Clase Producto
+
+export default class Producto {
+    constructor(nombre, imagen, precio, rareza, tipo, bonus) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.rareza = rareza;
+        this.tipo = tipo; 
+        this.bonus = bonus;
+    }
+
+    // FUNCIONES:
+
+    // 1. Formatea atributos en función de las necesidades el precio "Ej: de 950 a 9,50€".
+    formatearAtributos() {
+        return formatCurrency(this.precio);
+    }
+
+   // 2. Aplicar un descuento, devuelve el producto clonado
+    aplicarUnDescuento(valor) {
+
+        const nuevoPrecio = this.precio - (this.precio * valor / 100);
+        const clon = new Producto(
+            this.nombre,
+            this.imagen,
+            Math.round(nuevoPrecio), 
+            this.rareza,
+            this.tipo,
+            this.bonus
+        );
+        return clon;
+    }
+}
---
+++ modules/mercado.js
@@ -0,0 +1,36 @@
+// Mercado
+
+import Producto from '../clases/Producto.js';
+
+// Listado de los productos del mercado
+const LISTA_PRODUCTOS_ORIGINAL = [
+
+    new Producto("Espada", "img", 1200, RAREZA.RARA, TIPO_PRODUCTO.ARMA, 25),
+    new Producto("Hacha", "img", 1500, RAREZA.LEGENDARIA, TIPO_PRODUCTO.ARMA, 40),
+    new Producto("Armadura de Escamas", "img", 900, RAREZA.COMUN, TIPO_PRODUCTO.ARMADURA, 15),
+    new Producto("Casco del Dragón", "img", 1000, RAREZA.RARA, TIPO_PRODUCTO.ARMADURA, 20),
+    new Producto("Fruto de Dragón", "img", 50, RAREZA.COMUN, TIPO_PRODUCTO.CONSUMIBLE, 20),
+    new Producto("Poción", "img", 80, RAREZA.RARA, TIPO_PRODUCTO.CONSUMIBLE, 40),
+   
+];
+
+// Buscar producto
+export function getListaProductosOriginal() {
+    return LISTA_PRODUCTOS_ORIGINAL.map(p => p); 
+}
+
+// Filtrar productos en función de la rareza.
+export function filtrarProductos(rareza) {
+    return LISTA_PRODUCTOS_ORIGINAL.filter(p => p.rareza === rareza);
+}
+
+// Funcion aplica el descuento, si coincide con la rareza o tipo
+export function aplicarDescuentoPorCriterio(criterio, descuento) {
+
+    return LISTA_PRODUCTOS_ORIGINAL.map(producto => {
+        if (producto.rareza === criterio || producto.tipo === criterio) {
+            return producto.aplicarUnDescuento(descuento); 
+        }
+        return producto;
+    });
+}
